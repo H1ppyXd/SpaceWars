b558f5f59022003370dba68a70fc98ef152f9875
H1ppyXd <staspashkov2003@mail.ru>
2020-12-24 16:31:42 +0300

    Test

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
 import pygame
 
 # Сделать Класс Героя
+class Hero:
+    def __init__(self, name):
+        self.name = name
+
+
 pygame.init()
 size = wigth, height = 800, 600
 screen = pygame.display.set_mode(size)
