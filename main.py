3f3faf4b41bc751512e401f64a313934a5677fd7
H1ppyXd <staspashkov2003@mail.ru>
2020-12-24 16:19:53 +0300

    Test

--- main.py
+++ main.py
@@ -1,1 +1,14 @@
-print('hello')
+import pygame
+
+
+pygame.init()
+size = wigth, height = 800, 600
+screen = pygame.display.set_mode(size)
+pygame.display.set_caption('SpaceWars')
+running = True
+
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+    pygame.display.flip()
