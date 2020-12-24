944fb5aefe6561720a7bfcc37330cccd9021681d
H1ppyXd <staspashkov2003@mail.ru>
2020-12-24 16:44:48 +0300

    Test

--- main.py
+++ main.py
@@ -1,20 +1,12 @@
 import pygame
 
-# Сделать Класс Героя
-
-class Hero:
-    def __init__(self, name):
-        self.name = name
-
-
-class Enemy:
-    def __init__(self):
-        pass
 
 pygame.init()
 size = wigth, height = 800, 600
 screen = pygame.display.set_mode(size)
 pygame.display.set_caption('SpaceWars')
+
+evil_sprites = pygame.sprite.Group()
 
 running = True
 while running:
